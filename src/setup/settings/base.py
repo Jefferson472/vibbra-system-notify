c0c64fc086389b7bc353066c94493585889bf908
Jefferson Miranda <jefferson472@hotmail.com>
2024-07-28 10:33:26 -0300

    add logo and favicon

--- src/setup/settings/base.py
+++ src/setup/settings/base.py
@@ -116,12 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "")]
+DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(DATA_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(DATA_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
